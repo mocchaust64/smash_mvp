c31656eb6314b2710a6841cf74804d9f4ba7a0cc
Smoke <60354245+mocchaust64@users.noreply.github.com>
2026-09-10 20:38:45 +0700

    feat: add game event bus

---
+++ scripts/game_events.gd
@@ -0,0 +1,8 @@
+extends Node
+
+signal ball_cleared(position: Vector3)
+signal block_broken(position: Vector3)
+signal shot_fired(position: Vector3)
+signal wrong_ammo(position: Vector3)
+signal level_won(stars: int)
+signal level_lost
